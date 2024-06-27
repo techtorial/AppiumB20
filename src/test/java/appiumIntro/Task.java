8ee1cd022ebb8fe4558c507bca2a654e1f94c133
techtorial <50930535+techtorial@users.noreply.github.com>
2024-06-26 21:07:46 -0500

    Latest

---
+++ src/test/java/appiumIntro/Task.java
@@ -0,0 +1,18 @@
+package appiumIntro;
+
+public class Task {
+
+
+    /*
+1-Setup your Automation
+2-Click + Button
+3-Send your Name and Task
+4-Validate Important Task is visible,enable and not selected.
+5-Select important Task box button
+6-Click Ok Button and Validate Name and Task shows up correctly on the list
+7-Click X button and Ok Button(uio)
+8-Validate the message is "The list does not contain any element !"
+9-Proud of yourself
+ */
+
+}
